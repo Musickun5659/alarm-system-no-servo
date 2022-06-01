7298944d3c93b334bfbaaf5bb78b3de414a80695
Musickun5659 <103948827+Musickun5659@users.noreply.github.com>
2022-06-01 11:38:48 -0400

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,20 +1,26 @@
-function disablealarm () {
-    pins.digitalWritePin(DigitalPin.P2, 0)
-}
-function alarm () {
-    for (let index = 0; index < 100; index++) {
-        pins.digitalWritePin(DigitalPin.P2, 1)
-    }
-}
+let alarmon = 0
+let alarmdisabled = 0
 let _switch = 0
 let servo_value = 0
 pins.setAudioPin(AnalogPin.P2)
+music.setTempo(220)
 basic.forever(function () {
-    servo_value = pins.digitalReadPin(DigitalPin.P0)
+    servo_value = pins.analogReadPin(AnalogPin.P0)
     _switch = pins.digitalReadPin(DigitalPin.P1)
-    if (servo_value == 1) {
-        disablealarm()
-    } else if (_switch == 1) {
-    	
+    if (servo_value == 318) {
+        alarmdisabled = 1
+    } else if (_switch == 0) {
+        alarmon = 1
+    }
+    if (alarmdisabled) {
+        music.setVolume(0)
+    } else if (alarmon) {
+        for (let index = 0; index < 100; index++) {
+            music.playTone(262, music.beat(BeatFraction.Whole))
+            music.rest(music.beat(BeatFraction.Whole))
+        }
+        if (alarmdisabled) {
+            music.setVolume(0)
+        }
     }
 })
